2002de1d6a8dcc3ea5aa763fdd3c15a3943a6c79
FTC User <ftc.student@a-pcsd.net>
2021-10-29 11:56:21 -0500

    Corrected the diameter of the wheels in MecanumAutonomous.java -Jacob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAutonomous.java
@@ -26,7 +26,7 @@ public class MecanumAutonomous extends LinearOpMode {
     // Sets the motor specifications as variables
     static final double COUNTS_PER_MOTOR_REV = 960;
     static final double DRIVE_GEAR_REDUCTION = 1.0;
-    static final double WHEEL_DIAMETER_INCHES = 2.95276;
+    static final double WHEEL_DIAMETER_INCHES = 4.0;
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.6;
     static final double TURN_SPEED = 0.6;
