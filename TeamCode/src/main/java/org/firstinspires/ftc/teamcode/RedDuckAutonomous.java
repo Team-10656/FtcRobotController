ec94f83aaa8b8bf8179633caea99d3ed76a7b6b6
FTC User <ftc.student@a-pcsd.net>
2021-12-10 15:12:37 -0600

    Fixed the inches in RedDuckAutonomous.java -Jacob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedDuckAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedDuckAutonomous.java
@@ -143,7 +143,7 @@ public class RedDuckAutonomous extends LinearOpMode {
 
         encoderDrive(TURN_SPEED, 45, -45, .475);
 
-        encoderDrive(DRIVE_SPEED, 20, 20, 5.7);
+        encoderDrive(DRIVE_SPEED, 125, 125, 5.7);
 
         arm.setPower(0);
     }
