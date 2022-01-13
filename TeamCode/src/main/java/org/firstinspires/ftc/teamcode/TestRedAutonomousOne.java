05c3632b2839a49e6074571065590c45d66084eb
FTC User <ftc.student@a-pcsd.net>
2022-01-13 15:43:29 -0600

    Fixed the opmode name of TestRedAutonomousOne.java -Jacob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRedAutonomousOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRedAutonomousOne.java
@@ -39,7 +39,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous(name="Red One", group="Red")
+@Autonomous(name="Test Red One", group="Red")
 //@Disabled
 public class TestRedAutonomousOne extends LinearOpMode {
     // Sets the runtime variable to the elapsed time within autonomous
